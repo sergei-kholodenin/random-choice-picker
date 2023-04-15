20d31970c07a26ee5514c006bec1531d4e70b72a
Sergei Kholodenin <65288088+sergei-kholodenin@users.noreply.github.com>
2023-04-15 10:12:41 +0300

    add random choice function

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ textarea.focus();
 
 textarea.addEventListener("keyup", (e) => {
     createTags(e.target.value);
+
+    if (e.key === "Enter") {
+        setTimeout(() => {
+            e.target.value = '';
+        }, 10)
+
+        randomSelect();
+    }
 });
 
 function createTags(input) {
@@ -16,5 +24,36 @@ function createTags(input) {
         tagEl.textContent = tag;
         tagsEl.appendChild(tagEl);
     })
+}
 
+function randomSelect() {
+    const times = 30;
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag();
+        highlightTag(randomTag);
+        setTimeout(() => {
+            unHighlightTag(randomTag);
+        }, 100)
+    }, 100);
+    setTimeout(() => {
+        clearInterval(interval);
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag();
+            highlightTag(randomTag);
+        }, 100)
+    }, times * 100)
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll(".tag");
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+    tag.classList.add("highlight");
+}
+
+function unHighlightTag(tag) {
+    tag.classList.remove("highlight");
 }
